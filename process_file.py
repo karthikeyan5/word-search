b258cf0b927b3ff4ddd1514c92ffc1666e2a508b
jaideep8 <v.jaideep8@gmail.com>
2016-08-30 00:50:50 +0530

    first commit

--- process_file.py
+++ process_file.py
@@ -1,1 +1,16 @@
-# this adds words to the dictionary from single text file passed to it. 
+# this adds words to the dictionary from single text file passed to it.
+import find_word.py as fw
+def file_processing(fname,fp):
+    L=[]
+    for line in fp:
+        for word in line.split():
+            if word not in L:
+                filenames_list = fw.find_word()
+                if filenames_list == "None":
+                    dict[word[0:1]] = word
+                    dict[word[0:1]][word] = fname
+                else:
+                    filenames_list.append(fname)
+                    dict[word[0:1]][word] = filenames_list.append(fname)
+                    L.append(word)
+
